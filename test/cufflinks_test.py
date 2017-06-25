f7497b4d27a60a806f3ea60c55c783fb0e21c934
Arfath Pasha <arfathpasha@gmail.com>
2017-06-25 16:24:41 -0400

    redo of cufflinks test setup

--- test/cufflinks_test.py
+++ test/cufflinks_test.py
@@ -4,10 +4,11 @@ import os  # noqa: F401
 import json  # noqa: F401
 import time
 import shutil
-from string import Template
-from ReadsAlignmentUtils.ReadsAlignmentUtilsClient import ReadsAlignmentUtils
+
 from GenomeFileUtil.GenomeFileUtilClient import GenomeFileUtil
 from ReadsUtils.ReadsUtilsClient import ReadsUtils
+from ReadsAlignmentUtils.ReadsAlignmentUtilsClient import ReadsAlignmentUtils
+from DataFileUtil.DataFileUtilClient import DataFileUtil
 
 from os import environ
 try:
@@ -17,6 +18,7 @@ except:
 
 from pprint import pprint  # noqa: F401
 
+from kb_cufflinks.core.cufflinks_utils import CufflinksUtils
 from kb_cufflinks.kb_cufflinksImpl import kb_cufflinks
 from kb_cufflinks.kb_cufflinksServer import MethodContext
 from kb_cufflinks.authclient import KBaseAuth as _KBaseAuth
@@ -56,6 +58,33 @@ class CufflinksTest(unittest.TestCase):
         cls.scratch = cls.cfg['scratch']
         cls.callback_url = os.environ['SDK_CALLBACK_URL']
 
+        cls.gfu = GenomeFileUtil(cls.callback_url)
+        cls.dfu = DataFileUtil(cls.callback_url)
+        cls.ru = ReadsUtils(cls.callback_url)
+        cls.rau = ReadsAlignmentUtils(cls.callback_url, service_ver='dev')
+
+        cls.cufflinks_runner = CufflinksUtils(cls.cfg)
+
+        #suffix = int(time.time() * 1000)
+        suffix = 1001
+        cls.wsName = "test_kb_cufflinks_" + str(suffix)
+
+        try:
+            # reuse existing (previously torn down) workspace
+            cls.wsClient.undelete_workspace({'workspace': cls.wsName})
+            print('reusing old workspace...')
+        except:
+            try:
+                # create if workspace does not exist
+                cls.wsClient.create_workspace({'workspace': cls.wsName})
+            except:
+                # get workspace if it exists and was not previously deleted (previously not torn down)
+                ws_info = cls.wsClient.get_workspace_info({'workspace': cls.wsName})
+                print("creating new workspace: " + str(ws_info))
+
+        cls.prepare_data()
+
+        '''
         input_meta_data_filename = '/kb/module/test/metadata/cufflinks_input.json'
 
         with open(input_meta_data_filename,'r') as infile:
@@ -77,7 +106,7 @@ class CufflinksTest(unittest.TestCase):
             ws_info = cls.ws.get_workspace_info({'workspace': cls.wsName})
             print("creating new workspace: " + str(ws_info))
 
-        '''
+        
         # upload genbank file
         print('uploading genbank file to workspace...')
         INPUT_DATA_DIR = "/kb/module/test/data/"
@@ -133,6 +162,114 @@ class CufflinksTest(unittest.TestCase):
             cls.wsClient.delete_workspace({'workspace': cls.wsName})
             print('Test workspace was deleted')
 
+    @classmethod
+    def prepare_data(cls):
+        # upload genome object
+        genbank_file_name = 'minimal.gbff'
+        genbank_file_path = os.path.join(cls.scratch, genbank_file_name)
+        shutil.copy(os.path.join('data', genbank_file_name), genbank_file_path)
+
+        genome_object_name = 'test_Genome'
+        cls.genome_ref = cls.gfu.genbank_to_genome({'file': {'path': genbank_file_path},
+                                                    'workspace_name': cls.wsName,
+                                                    'genome_name': genome_object_name
+                                                    })['genome_ref']
+
+        # upload reads object
+        reads_file_name = 'Sample1.fastq'
+        reads_file_path = os.path.join(cls.scratch, reads_file_name)
+        shutil.copy(os.path.join('data', reads_file_name), reads_file_path)
+
+        reads_object_name_1 = 'test_Reads_1'
+        cls.reads_ref_1 = cls.ru.upload_reads({'fwd_file': reads_file_path,
+                                               'wsname': cls.wsName,
+                                               'sequencing_tech': 'Unknown',
+                                               'interleaved': 0,
+                                               'name': reads_object_name_1
+                                               })['obj_ref']
+
+        reads_object_name_2 = 'test_Reads_2'
+        cls.reads_ref_2 = cls.ru.upload_reads({'fwd_file': reads_file_path,
+                                               'wsname': cls.wsName,
+                                               'sequencing_tech': 'Unknown',
+                                               'interleaved': 0,
+                                               'name': reads_object_name_2
+                                               })['obj_ref']
+
+        # upload alignment object
+        alignment_file_name = 'accepted_hits.bam'
+        alignment_file_path = os.path.join(cls.scratch, alignment_file_name)
+        shutil.copy(os.path.join('data', alignment_file_name), alignment_file_path)
+
+        alignment_object_name_1 = 'test_Alignment_1'
+        cls.condition_1 = 'test_condition_1'
+        cls.alignment_ref_1 = cls.rau.upload_alignment(
+            {'file_path': alignment_file_path,
+             'destination_ref': cls.wsName + '/' + alignment_object_name_1,
+             'read_library_ref': cls.reads_ref_1,
+             'condition': cls.condition_1,
+             'library_type': 'single_end',
+             'assembly_or_genome_ref': cls.genome_ref
+             })['obj_ref']
+
+        alignment_object_name_2 = 'test_Alignment_2'
+        cls.condition_2 = 'test_condition_2'
+        cls.alignment_ref_2 = cls.rau.upload_alignment(
+            {'file_path': alignment_file_path,
+             'destination_ref': cls.wsName + '/' + alignment_object_name_2,
+             'read_library_ref': cls.reads_ref_2,
+             'condition': cls.condition_2,
+             'library_type': 'single_end',
+             'assembly_or_genome_ref': cls.genome_ref
+             })['obj_ref']
+
+        # upload sample_set object
+        workspace_id = cls.dfu.ws_name_to_id(cls.wsName)
+        sample_set_object_name = 'test_Sample_Set'
+        sample_set_data = {
+            'sampleset_id': sample_set_object_name,
+            'sampleset_desc': 'test sampleset object',
+            'Library_type': 'SingleEnd',
+            'condition': [cls.condition_1, cls.condition_2],
+            'domain': 'Unknown',
+            'num_samples': 2,
+            'platform': 'Unknown'}
+        save_object_params = {
+            'id': workspace_id,
+            'objects': [{
+                'type': 'KBaseRNASeq.RNASeqSampleSet',
+                'data': sample_set_data,
+                'name': sample_set_object_name
+            }]
+        }
+
+        dfu_oi = cls.dfu.save_objects(save_object_params)[0]
+        cls.sample_set_ref = str(dfu_oi[6]) + '/' + str(dfu_oi[0]) + '/' + str(dfu_oi[4])
+
+        # upload alignment_set object
+        object_type = 'KBaseRNASeq.RNASeqAlignmentSet'
+        alignment_set_object_name = 'test_Alignment_Set'
+        alignment_set_data = {
+            'genome_id': cls.genome_ref,
+            'read_sample_ids': [reads_object_name_1, reads_object_name_2],
+            'mapped_rnaseq_alignments': [{reads_object_name_1: alignment_object_name_1},
+                                         {reads_object_name_2: alignment_object_name_2}],
+            'mapped_alignments_ids': [{reads_object_name_1: cls.alignment_ref_1},
+                                      {reads_object_name_2: cls.alignment_ref_2}],
+            'sample_alignments': [cls.alignment_ref_1, cls.alignment_ref_2],
+            'sampleset_id': cls.sample_set_ref}
+        save_object_params = {
+            'id': workspace_id,
+            'objects': [{
+                'type': object_type,
+                'data': alignment_set_data,
+                'name': alignment_set_object_name
+            }]
+        }
+
+        dfu_oi = cls.dfu.save_objects(save_object_params)[0]
+        cls.alignment_set_ref = str(dfu_oi[6]) + '/' + str(dfu_oi[0]) + '/' + str(dfu_oi[4])
+
     def getWsClient(self):
         return self.__class__.wsClient
 
@@ -154,26 +291,10 @@ class CufflinksTest(unittest.TestCase):
 
     # NOTE: According to Python unittest naming rules test method names should start from 'test'. # noqa
     def test_cufflinks(self):
-
-        # upload alignment file
-        '''
         params = {
-                  'destination_ref': self.getWsName() + '/accepted_hits.bam',
-                  'file_path': '/kb/module/work/tmp/accepted_hits.bam',
-                  'validate': 'True',
-                  'read_library_ref': self.getWsName() + '/Ath_hy5_rep1.fastq',
-                  'assembly_or_genome_ref': self.getWsName() + '/KBase_derived_Athaliana_PhytozomeV11_TAIR10',
-                  'condition': 'test_condition'
-                 }
-
-        rau = ReadsAlignmentUtils(url=self.__class__.callback_url, token=self.getContext()['token'], service_ver='dev')
-        ref = rau.upload_alignment(params=params)
-        '''
-
-        params = {
-            "ws_id": "Cufflinks_test_arfath",
-            "sample_alignment_ref" : "accepted_hits.bam",
-            "genome_ref" : "KBase_derived_Athaliana_PhytozomeV11_TAIR10",
+            "ws_id": self.getWsName(),
+            "sample_alignment_ref" : self.alignment_ref_1,
+            "genome_ref" : self.__class__.genome_ref,
             "min-intron-length" : 50,
             "max-intron-length" : 300000,
             "overhang-tolerance" : 8,
