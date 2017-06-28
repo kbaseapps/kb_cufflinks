4f206d0461c7f58b4ca89b7141a1558ce1dbbeb4
U Ganapathy <umaganapathyswork@gmail.com>
2017-06-28 17:26:18 -0400

    Added ExpressionUtils and related code

--- test/cuffdiff_test.py
+++ test/cuffdiff_test.py
@@ -3,8 +3,10 @@ import unittest
 import os  # noqa: F401
 import json  # noqa: F401
 import time
+import inspect
 import requests
 import shutil
+from datetime import datetime
 from string import Template
 
 from os import environ
@@ -79,19 +81,31 @@ class kb_cufflinksTest(unittest.TestCase):
     def getContext(self):
         return self.__class__.ctx
 
+    def check_files(self, new_dir, orig_dir):
+
+        self.assertEqual(len(os.listdir(new_dir)),
+                         len(os.listdir(orig_dir)))
+
+        for new_file in os.listdir(new_dir):
+            new_file_path = os.path.join(new_dir, new_file)
+            orig_file_path = os.path.join(orig_dir, new_file)
+
+            self.assertEqual(self.getSize(new_file_path), self.getSize(orig_file_path))
+            self.assertEqual(self.md5(new_file_path), self.md5(orig_file_path))
+
+            print("Files checked: " + new_file_path + ', ' + orig_file_path)
+
     # NOTE: According to Python unittest naming rules test method names should start from 'test'. # noqa
     def test_cuffdiff_success(self):
-        # Prepare test objects in workspace if needed using
-        # self.getWsClient().save_objects({'workspace': self.getWsName(),
-        #                                  'objects': []})
-        #
-        # Run your method by
-        # ret = self.getImpl().your_method(self.getContext(), parameters...)
-        #
-        # Check returned data with
-        # self.assertEqual(ret[...], ...) or other unittest methods
+        """
+        Input object: downsized_AT_reads_tophat_AlignmentSet_cufflinks_ExpressionSet (4389/46/1)
+        Expected output object: downsized_AT_reads_cuffdiff_output (4389/48/1)
+        Files in output object should be the same as in expected output object
+        """
+        input_obj_ref = '4389/18/2'
+        expected_obj_ref = '4389/48/1'
 
-        params = {'expressionset_ref': '22254/32/1',
+        params = {'expressionset_ref': input_obj_ref,
                   'workspace_name': self.getWsName(),
                   'diff_expression_obj_name': 'test_output_diffexp',
                   'filtered_expression_matrix_name': 'test_output_expmatrix',
@@ -101,23 +115,110 @@ class kb_cufflinksTest(unittest.TestCase):
         retVal = self.getImpl().run_Cuffdiff(self.ctx, params)[0]
 
         inputObj = self.dfu.get_objects(
-            {'object_refs': [retVal.get('diff_expression_obj_ref')]})['data'][0]
+            {'object_refs': [input_obj_ref]})['data'][0]
 
-        obj = self.dfu.get_objects(
+        print("============ INPUT EXPRESSION SET OBJECT ==============")
+        pprint(inputObj)
+        print("==========================================================")
+
+        outputObj = self.dfu.get_objects(
             {'object_refs': [retVal.get('diff_expression_obj_ref')]})['data'][0]
 
         print("============ DIFFERENTIAL EXPRESSION OUTPUT ==============")
-        pprint(obj)
+        pprint(outputObj)
         print("==========================================================")
 
-        self.assertEqual(obj['info'][2].startswith('KBaseRNASeq.RNASeqDifferentialExpression'), True)
-        d = obj['data']
-        self.assertEqual(d['genome_id'], inputObj['data']['genome_id'])
-        self.assertEqual(d['expressionSet_id'], inputObj['data']['expressionSet_id'])
-        self.assertEqual(d['alignmentSet_id'], inputObj['data']['alignmentSet_id'])
-        self.assertEqual(d['sampleset_id'], inputObj['data']['sampleset_id'])
+        self.assertEqual(outputObj['info'][2].startswith('KBaseRNASeq.RNASeqDifferentialExpression'), True)
+        inputData = inputObj['data']
+        outputData = outputObj['data']
+        self.assertEqual(outputData['genome_id'], inputData['genome_id'])
+        self.assertEqual(outputData['expressionSet_id'], input_obj_ref)
+        self.assertEqual(outputData['alignmentSet_id'], inputData['alignmentSet_id'])
+        self.assertEqual(outputData['sampleset_id'], inputData['sampleset_id'])
 
-        f = d['file']
-        result_dir = retVal['result_directory']
-        result_file = os.path.split(result_dir)[1] + '.zip'
-        self.assertEqual(f['file_name'], result_file)
+        outputFile = outputData['file']
+        output_dir = retVal['result_directory']
+        output_zipfile = os.path.split(output_dir)[1] + '.zip'
+        self.assertEqual(outputFile['file_name'], output_zipfile)
+
+        """
+        Get files from expected object ref
+        """
+        '''
+        timestamp = int((datetime.utcnow() - datetime.utcfromtimestamp(0)).total_seconds() * 1000)
+        expected_dir = os.path.join(self.scratch, 'expected_' + str(timestamp))
+        os.mkdir(expected_dir)
+
+        expectedObj = self.dfu.get_objects(
+            {'object_refs': [expected_obj_ref]})['data'][0]
+        expectedFile = expectedObj['data']['file']
+        expectedFile_ret = self.dfu.shock_to_file({
+                                                   'shock_id': expectedFile['id'],
+                                                   'file_path': expected_dir,
+                                                   'unpack': 'unpack'
+                                                   })
+        self.check_files(output_dir, expected_dir)
+        '''
+
+    '''
+    def test_no_workspace_param(self):
+
+        self.run_error(
+            [{'expressionset_ref': '5264/2/2'}], 'workspace_name parameter is required')
+
+    def test_no_workspace_name(self):
+
+        self.run_error(
+            [{'libfile_library': 'foo'}], 'workspace_name parameter is required', wsname='None')
+
+    def test_bad_workspace_name(self):
+
+        self.run_error([{'libfile_library': 'foo'}], 'Invalid workspace name bad|name',
+                       wsname='bad|name')
+
+    def test_non_extant_workspace(self):
+
+        self.run_error(
+            [{'libfile_library': 'foo'}], 'Object foo cannot be accessed: No workspace with name ' +
+                                          'Ireallyhopethisworkspacedoesntexistorthistestwillfail exists',
+            wsname='Ireallyhopethisworkspacedoesntexistorthistestwillfail',
+            exception=WorkspaceError)
+
+    def test_no_libs_param(self):
+
+        self.run_error(None, 'libfile_args parameter is required')
+
+    def test_non_extant_lib(self):
+
+        self.run_error(
+            [{'libfile_library': 'foo'}],
+            ('No object with name foo exists in workspace {} ' +
+             '(name {})').format(str(self.wsinfo[0]), self.wsinfo[1]),
+            exception=WorkspaceError)
+
+    def test_no_libs(self):
+
+        self.run_error([], 'At least one reads library must be provided')
+
+    def test_no_output_param(self):
+
+        self.run_error(
+            [{'libfile_library': 'foo'}], 'output_contigset_name parameter is required',
+            output_name=None)
+
+    def test_invalid_min_contig(self):
+
+        self.run_error(
+            [{'libfile_library': 'foo'}], 'min_contig must be of type int', wsname='fake', output_name='test-output',
+            min_contig_len='not an int!')
+
+    def run_error(self, params, error, exception=ValueError):
+
+        test_name = inspect.stack()[1][3]
+        print('\n***** starting expected fail test: ' + test_name + ' *****')
+
+        with self.assertRaises(exception) as context:
+            self.getImpl().run_cuffdiff(self.ctx, params[0])
+        self.assertEqual(error, str(context.exception.message))
+    '''
+
